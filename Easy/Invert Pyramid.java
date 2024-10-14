234165a2f3e3ebe331238fc3659c1bf1b31d153b
BhaveshKumar002 <123447145+BhaveshKumar002@users.noreply.github.com>
2024-10-14 08:59:25 +0530

    Create Invert Pyramid.java

---
+++ Easy/Invert Pyramid.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class InvertedPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        
+        for (int i = rows; i >= 1; i--) {
+       
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+           
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
