5a0ccdb7a7d2337b7105a9727a742f1eee1a5998
BhaveshKumar002 <123447145+BhaveshKumar002@users.noreply.github.com>
2024-10-17 08:57:57 +0530

    Create RansomNote.java

---
+++ Hard/RansomNote.java
@@ -0,0 +1,18 @@
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] letters = new int[26];
+        
+        for (char c : magazine.toCharArray()) {
+            letters[c - 'a']++;
+        }
+        
+        for (char c : ransomNote.toCharArray()) {
+            if (letters[c - 'a'] == 0) {
+                return false;
+            }
+            letters[c - 'a']--;
+        }
+        
+        return true;
+    }
+}
