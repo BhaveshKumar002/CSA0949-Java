bd5ef09659af4d8fb9382281e2fc4387ba40ba4b
BhaveshKumar002 <123447145+BhaveshKumar002@users.noreply.github.com>
2024-10-17 09:15:02 +0530

    Create InttoZero.java

---
+++ Hard/InttoZero.java
@@ -0,0 +1,14 @@
+public class ReduceToZero {
+    public int numberOfSteps(int num) {
+        int steps = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+}
