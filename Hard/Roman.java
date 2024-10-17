03701cc869349a89dcd221746b2c6eed13c838ca
BhaveshKumar002 <123447145+BhaveshKumar002@users.noreply.github.com>
2024-10-17 08:48:16 +0530

    Create Roman.java

---
+++ Hard/Roman.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+        
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int current = romanMap.get(s.charAt(i));
+            if (i < s.length() - 1 && current < romanMap.get(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+        return result;
+    }
+}
