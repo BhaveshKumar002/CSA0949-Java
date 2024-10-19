0445f4d628c06e71fe60417edc2f7386168d0815
BhaveshKumar002 <123447145+BhaveshKumar002@users.noreply.github.com>
2024-10-19 09:12:51 +0530

    Create StringProduct.java

---
+++ Medium/StringProduct.java
@@ -0,0 +1,29 @@
+public class StringProduct {
+    public static void main(String[] args) {
+        String num1 = "123";
+        String num2 = "456";
+        System.out.println(multiply(num1, num2));
+    }
+
+    public static String multiply(String num1, String num2) {
+        int len1 = num1.length(), len2 = num2.length();
+        int[] result = new int[len1 + len2];
+
+        for (int i = len1 - 1; i >= 0; i--) {
+            for (int j = len2 - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int sum = mul + result[i + j + 1];
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
