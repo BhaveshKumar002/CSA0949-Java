8a1081d7835522cf0474b6d5807a45b4cf9a5a04
BhaveshKumar002 <123447145+BhaveshKumar002@users.noreply.github.com>
2024-10-19 08:39:48 +0530

    Create Power.java

---
+++ Easy/Power.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PowerOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int n = sc.nextInt();
+
+        boolean result = isPowerOfThree(n);
+        System.out.println(result);
+    }
+
+    public static boolean isPowerOfThree(int n) {
+        if (n < 1) return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
