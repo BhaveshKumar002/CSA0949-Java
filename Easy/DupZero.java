450adaf4611c17466a9c0b4a4205f7d1fd6a2bb5
BhaveshKumar002 <123447145+BhaveshKumar002@users.noreply.github.com>
2024-10-19 08:42:29 +0530

    Create DupZero.java

---
+++ Easy/DupZero.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class DuplicateZeros {
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 3, 0, 4, 5, 0};
+        System.out.println("Original array: " + Arrays.toString(arr));
+        
+
