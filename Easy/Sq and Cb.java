11f1c57cd8e49d731efbb336c9ed98b66575d7e4
BhaveshKumar002 <123447145+BhaveshKumar002@users.noreply.github.com>
2024-10-19 08:32:59 +0530

    Create Sq and Cb.java

---
+++ Easy/Sq and Cb.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sqncb
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        double a;
+        System.out.println("Enter a number");
+        a=sc.nextDouble();
+        System.out.print("Square = "+a*a);
+        System.out.print("\n5Cube = "+a*a*a);
+    }
+}
